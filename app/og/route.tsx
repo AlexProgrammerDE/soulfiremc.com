3e4e9dec6397c5ff9b564ca2ed0683d7ec9527d0
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-07-15 16:11:23 +0200

    feat: update logo

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -44,20 +44,33 @@ export async function GET(req) {
         }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" style={{position: 'absolute', top: 70, left: 80}} height="80"
-             viewBox="0 0 120 120">
+             viewBox="0 0 200 200">
           <defs>
-            <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
-              <stop offset="0%" stopColor="#3289BF" stopOpacity="1"/>
-              <stop offset="50%" stopColor="#502CC4" stopOpacity="1"/>
-              <stop offset="100%" stopColor="#750B80" stopOpacity="1"/>
+            <linearGradient id="bgGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+              <stop offset="0%" stop-color="#2a2d3e"/>
+              <stop offset="100%" stop-color="#1c1e2a"/>
+            </linearGradient>
+            <linearGradient id="iconGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+              <stop offset="33%" stop-color="#00F3FF"/>
+              <stop offset="66%" stop-color="#33f2f5"/>
             </linearGradient>
           </defs>
-          <rect width="100%" height="100%" fill="url(#grad1)" rx="10%" ry="10%"/>
-          <g xmlSpace="preserve">
-            <path
-              stroke="white"
-              fill="white"
-              d="M27.78 95.03Q38.73 95.03 38.73 87.54Q38.73 85.24 37.76 83.62Q36.78 82 35.13 80.81Q33.47 79.62 31.35 78.76Q29.22 77.9 26.85 77.03Q24.11 76.1 21.66 74.91Q19.22 73.72 17.42 72.1Q15.62 70.48 14.57 68.25Q13.53 66.02 13.53 62.85Q13.53 56.3 17.99 52.62Q22.46 48.95 30.3 48.95Q34.84 48.95 38.55 49.92Q42.26 50.9 43.98 52.05L41.75 57.74Q40.24 56.8 37.25 55.9Q34.26 55 30.3 55Q28.29 55 26.56 55.43Q24.83 55.86 23.54 56.73Q22.24 57.59 21.48 58.92Q20.73 60.26 20.73 62.06Q20.73 64.07 21.52 65.44Q22.31 66.81 23.75 67.85Q25.19 68.9 27.1 69.76Q29.01 70.62 31.31 71.49Q34.55 72.78 37.25 74.08Q39.95 75.38 41.93 77.18Q43.91 78.98 44.99 81.46Q46.07 83.94 46.07 87.47Q46.07 94.02 41.28 97.55Q36.5 101.08 27.78 101.08Q24.83 101.08 22.35 100.68Q19.86 100.29 17.92 99.75Q15.98 99.21 14.57 98.6Q13.17 97.98 12.38 97.55L14.46 91.79Q16.12 92.73 19.5 93.88Q22.89 95.03 27.78 95.03ZM54.5 100L54.5 50.1L84.59 50.1L84.59 56.08L61.48 56.08L61.48 71.13L82 71.13L82 77.03L61.48 77.03L61.48 100Z"/>
+          <rect width="100%" height="100%" rx="30" ry="30" fill="url(#bgGradient)"/>
+          <g transform="translate(30,30)">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="140"
+              height="140"
+              viewBox="0 0 24 24"
+              fill="url(#iconGradient)"
+            >
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M12.963 2.286a.75.75 0 0 0-1.071-.136 9.742 9.742 0 0 0-3.539 6.176
+           7.547 7.547 0 0 1-1.705-1.715.75.75 0 0 0-1.152-.082A9 9 0 1 0 15.68 4.534
+           a7.46 7.46 0 0 1-2.717-2.248ZM15.75 14.25a3.75 3.75 0 1 1-7.313-1.172
+           c.628.465 1.35.81 2.133 1a5.99 5.99 0 0 1 1.925-3.546
+           3.75 3.75 0 0 1 3.255 3.718Z"/>
+            </svg>
           </g>
         </svg>
         <p
