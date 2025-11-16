b33c82587dc0568bad516a2b89089e52340a2bba
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-11-16 19:15:31 +0100

    fix: resolve warning

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,14 +1,10 @@
 import posthog from "posthog-js";
 
-const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-
-if (posthogKey) {
-  posthog.init(posthogKey, {
-    api_host: "/RELAY-KAWND",
-    ui_host: "https://eu.posthog.com",
-    defaults: "2025-05-24",
-    capture_exceptions: true, // capture exceptions using Error Tracking
-    debug: process.env.NODE_ENV === "development",
-    cookieless_mode: "always",
-  });
-}
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+  api_host: "/RELAY-KAWND",
+  ui_host: "https://eu.posthog.com",
+  defaults: "2025-05-24",
+  capture_exceptions: true, // capture exceptions using Error Tracking
+  debug: process.env.NODE_ENV === "development",
+  cookieless_mode: "always",
+});
