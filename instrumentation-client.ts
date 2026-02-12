b148e8ec019e36ba754a53d008a18823cbd8d005
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-02-12 11:13:04 +0100

    fix: switch to different api host

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,7 +1,7 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-  api_host: "/RELAY-KAWND",
+  api_host: "https://e.soulfiremc.com",
   ui_host: "https://eu.posthog.com",
   defaults: "2025-05-24",
   capture_exceptions: true, // capture exceptions using Error Tracking
