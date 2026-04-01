24bb95327a58f52732cdece86c10e6f792d7c9fc
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-04-01 23:22:50 +0200

    Update Discord URL for Localts account

--- src/lib/accounts-data.tsx
+++ src/lib/accounts-data.tsx
@@ -495,7 +495,7 @@ export const SHOPS: Shop[] = [
     logo: "/accounts/localts.png",
     url: "https://localts.store",
     websiteUrl: "https://localts.info",
-    discordUrl: "https://discord.gg/Q8HEd6S8ud",
+    discordUrl: "https://localts.store/discord",
     startDate: "Jun 2023",
     listings: {
       "nfa-accounts": {
