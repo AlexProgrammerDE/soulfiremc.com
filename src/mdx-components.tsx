8f560d47429d2b5ca510734844a8f75c44a6c841
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-04-08 14:53:12 +0200

    chore: improve mdx components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,15 +1,16 @@
+import { Callout } from "fumadocs-ui/components/callout";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
-import type { ImageZoomProps } from "fumadocs-ui/components/image-zoom";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import type { ComponentProps, FC } from "react";
 import { DonutCalculator } from "@/components/mdx/donut-calculator";
 import { Mermaid } from "@/components/mdx/mermaid";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: (props) => <ImageZoom {...(props as ImageZoomProps)} />,
+    img: ImageZoom as unknown as FC<ComponentProps<"img">>,
     // HTML `ref` attribute conflicts with `forwardRef`
     pre: ({ ref: _ref, ...props }) => (
       <CodeBlock {...props}>
@@ -18,6 +19,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ),
     Mermaid,
     DonutCalculator,
+    blockquote: Callout as unknown as FC<ComponentProps<"blockquote">>,
     ...components,
   };
 }
