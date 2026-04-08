426a236510e9a7c1f7215543f7eb8ee936ef8d90
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-04-08 15:25:24 +0200

    chore: update components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,8 +1,10 @@
 import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
 import { Callout } from "fumadocs-ui/components/callout";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import * as FilesComponents from "fumadocs-ui/components/files";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import { Step, Steps } from "fumadocs-ui/components/steps";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import type { ComponentProps, FC } from "react";
@@ -16,6 +18,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     Accordions,
     Step,
     Steps,
+    ...TabsComponents,
+    ...FilesComponents,
     blockquote: Callout as unknown as FC<ComponentProps<"blockquote">>,
     img: ImageZoom as unknown as FC<ComponentProps<"img">>,
     // HTML `ref` attribute conflicts with `forwardRef`
