12e4aa5acfdb64abd7c7430491a016befc6c81b7
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-04-13 19:49:57 +0200

    chore: add mock db url for prerendering

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as generatedAuthSchema from "./auth-schema";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL ?? "");
+const sql = neon(process.env.DATABASE_URL ?? "postgres://postgres:password@localhost:5432/soulfire");
 export const db = drizzle(sql, {
   schema: {
     ...generatedAuthSchema,
