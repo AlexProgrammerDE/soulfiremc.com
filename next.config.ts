48f2ce03dead44679f369fa9bb646f72b2e24bff
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-04-14 13:10:01 +0200

    Revert "chore: update external packages"
    This reverts commit d5ebc2cafb0dd981741391d7c72b0ed8a474acef.

--- next.config.ts
+++ next.config.ts
@@ -65,7 +65,7 @@ const config: NextConfig = {
   reactStrictMode: true,
   reactCompiler: true,
   cacheComponents: true,
-  serverExternalPackages: ["@takumi-rs/core"],
+  serverExternalPackages: ["@takumi-rs/image-response"],
   experimental: {
     viewTransition: true,
   },
